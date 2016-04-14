d0905d69ef0df617f9b4619a8bd847b97bfca2d4
cignoir <cignoir@gmail.com>
2016-04-14 15:40:45 +0900

    Add spec for Position#initialize

--- spec/gingham/position_spec.rb
+++ spec/gingham/position_spec.rb
@@ -4,4 +4,14 @@ describe Gingham::Position do
   it 'is defined' do
     expect{ Gingham::Position }.not_to raise_error
   end
+
+  describe '#initialize' do
+    let(:pos) { Gingham::Position.new(1, 2, 3) }
+
+    it 'should be initialized' do
+      expect(pos.x).to eq 1
+      expect(pos.y).to eq 2
+      expect(pos.z).to eq 3
+    end
+  end
 end
