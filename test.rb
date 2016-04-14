0617f34d6d9af7bc8407b1301e9428707c50561d
cignoir <cignoir@gmail.com>
2016-04-14 10:39:37 +0900

    Remove a needless file

--- test.rb
+++
@@ -1,6 +0,0 @@
-require 'gingham'
-
-space = Gingham::Space.new(5, 5, 3)
-from = Gingham::Waypoint.new(space.cells[2][1][1], 8)
-to = Gingham::Waypoint.new(space.cells[4][4][1], 8)
-Gingham::PathFinder.find_skill_path(space, from, to)
